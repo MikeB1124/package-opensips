35dde09e87b1615285c7ec5fa3c351f714051d26
MikeB1124 <balianmichael@gmail.com>
2025-03-01 22:42:38 -0800

    modify opensips installation

--- configure-opensips.sh
+++ configure-opensips.sh
@@ -1,43 +1,26 @@
+#!/bin/bash
+
+set -e  # Exit on error
+
+# Update package lists and install dependencies
 apt update -y
 apt install -y \
-    git \
-    make \
-    gcc \
-    bison \
-    flex \
-    pkg-config \
-    openssl \
-    libssl-dev \
-    libsctp-dev \
-    libmariadb-dev \
-    libmariadb-dev-compat \
-    libpq-dev \
-    unixodbc-dev \
-    libexpat1-dev \
-    libxml2-dev \
-    libxmlrpc-core-c3-dev \
-    libperl-dev \
-    libsnmp-dev \
-    libldap2-dev \
-    libconfuse-dev \
-    libncurses5-dev \
-    m4 \
-    gawk \
-    sed \
-    tar \
-    gzip \
-    libevent-dev \
-    libpcre2-dev \
-    libncurses5-dev \
-    libncursesw5-dev \
-    mariadb-server \
-    libpcre3-dev
+    git make gcc bison flex pkg-config openssl libssl-dev \
+    libsctp-dev libmariadb-dev libmariadb-dev-compat libpq-dev unixodbc-dev \
+    libexpat1-dev libxml2-dev libxmlrpc-core-c3-dev libperl-dev libsnmp-dev \
+    libldap2-dev libconfuse-dev libncurses5-dev libncursesw5-dev libevent-dev \
+    libpcre2-dev libpcre3-dev m4 gawk sed tar gzip mariadb-server
 
-sudo systemctl start mariadb
+# Start MariaDB
+systemctl start mariadb
 
-git clone --recurse-submodules https://github.com/OpenSIPS/opensips.git -b 3.3 opensips-3.3 /tmp/opensips
-cd /tmp/opensips
-#nned to enable my_sql module
+# Clone OpenSIPS repo
+OPENSiPS_DIR="/tmp/opensips"
+if [ -d "$OPENSiPS_DIR" ]; then rm -rf "$OPENSiPS_DIR"; fi
+git clone --recurse-submodules https://github.com/OpenSIPS/opensips.git -b 3.3 "$OPENSiPS_DIR"
+cd "$OPENSiPS_DIR"
+
+# Build and install OpenSIPS with required modules
 make all
 make install
 make modules=modules/tls_mgm install
@@ -46,11 +29,13 @@ make modules=modules/sipcapture install
 make modules=modules/freeswitch install
 make modules=modules/freeswitch_scripting install
 
-
-
-#opensips-cli
-curl https://apt.opensips.org/opensips-org.gpg -o /usr/share/keyrings/opensips-org.gpg
+# Install OpenSIPS CLI
+curl -fsSL https://apt.opensips.org/opensips-org.gpg -o /usr/share/keyrings/opensips-org.gpg
 echo "deb [signed-by=/usr/share/keyrings/opensips-org.gpg] https://apt.opensips.org bookworm cli-nightly" >/etc/apt/sources.list.d/opensips-cli.list
-apt install opensips-cli -y
+apt update -y
+apt install -y opensips-cli
 
+# Create OpenSIPS database
 opensips-cli -x database create
+
+echo "OpenSIPS installation completed successfully."
